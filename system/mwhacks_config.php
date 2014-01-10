194bcf3e2b1d69866c555c877c8699fce27a0166
inc2734 <inc2734@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-10 15:25:34 +0000

    Tagging version 0.4.0
    git-svn-id: https://plugins.svn.wordpress.org/mw-wp-hacks/tags/0.4.0@836188 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ system/mwhacks_config.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Name: MW Hacks Config
+ * URI: http://2inc.org
+ * Description: 設定値・規定値
+ * Version: 1.0.0
+ * Author: Takashi Kitajima
+ * Author URI: http://2inc.org
+ * Created : December 15, 2013
+ * Modified:
+ * License: GPL2
+ *
+ * Copyright 2013 Takashi Kitajima (email : inc@2inc.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+class MWHACKS_Config {
+
+	/**
+	 * NAME
+	 * プラグイン識別子
+	 */
+	const NAME = 'mw-wp-hacks';
+
+	/**
+	 * DOMAIN
+	 * ネームスペース
+	 */
+	const DOMAIN = 'mw-wp-hacks';
+}
