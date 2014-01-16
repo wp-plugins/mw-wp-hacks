5e1d2758f0429c2b7aa58e84560086196ee2478a
inc2734 <inc2734@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-16 08:14:06 +0000

    Version up. Ver 0.4.1
    git-svn-id: https://plugins.svn.wordpress.org/mw-wp-hacks/trunk@839522 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mw-wp-hacks.php
+++ mw-wp-hacks.php
@@ -3,13 +3,13 @@
  * Plugin Name: MW WP Hacks
  * Plugin URI: http://2inc.org
  * Description: MW WP Hacks is plugin to help with development in WordPress.
- * Version: 0.4.0
+ * Version: 0.4.1
  * Author: Takashi Kitajima
  * Author URI: http://2inc.org
  * Text Domain: mw-wp-hacks
  * Domain Path: /languages/
  * Created : September 30, 2013
- * Modified: January 10, 2013
+ * Modified: January 15, 2013
  * License: GPL2
  *
  * Copyright 2014 Takashi Kitajima (email : inc@2inc.org)
